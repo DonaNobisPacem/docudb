301eda024a92d815680673189429b55fa91742fb
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-04-05 14:10:22 +0900

    removed received by validate

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
 	validates :control, presence: true, uniqueness: true
 	validates :office, presence: true
 	validates :date, presence: true
-	validates :received_by, presence: true
+	#validates :received_by, presence: true
 	validates :subject, presence: true
 	validates :outgoing, inclusion: [true, false]
 
